d4b354f89578e8c79c38826a41135290ae1f7062
Claudia <claudiafontsere@gmail.com>
2015-02-24 20:19:20 +0100

    some advances in blast

--- claudia/test1.py
+++ claudia/test1.py
@@ -3,39 +3,31 @@ from Bio.Blast import NCBIXML
 from Bio import SeqIO
 from Bio import Seq
 import sys
-import re
 
 def doBlast (fastafile):
 	handle = open(fastafile)
 	
 	for record in SeqIO.parse(handle, "fasta") :
-		result = NCBIWWW.qblast("blastp", "swissprot", record.seq)
+		result = NCBIWWW.qblast("blastp", "nr", record.seq)
 		blastfile = open("blast.xml", "w")
 		blastfile.write(result.read())
 		blastfile.close()	
 	handle.close()
 
-
 def selectProt(blastxml):
 	result = open(blastxml, "r")
 	out = open("candidates.blast", "w")
-	evalue = 0.00001
-	p = re.compile( "Precursor[^\[]*\[([^\]]*)\]")
 	for n in NCBIXML.parse(result):
 		for alignment in n.alignments:
 			for hsp in alignment.hsps:
-				if hsp.expect < evalue:
+	#			if hsp.expect < 0.1:
 					out.write("****Alignment****\n")
-					out.write("Hit id: %s \n" %alignment.hit_id)
-					m = p.findall(alignment.hit_def)
-					out.write ("Hit specie: %s \n" %m)
-					out.write("Sequence: "+str(alignment.title[0:50])+"\n")
+					out.write("Sequence: "+str(alignment.title)+"\n")
 					out.write("Length: "+str(alignment.length)+"\n")
 					out.write("E-value: "+str(hsp.expect)+"\n")
-					out.write("Score: "+str(hsp.score)+"\n")
-# 					out.write(str(hsp.query)+"\n")
-# 					out.write(str(hsp.match) + "...\n")
-					out.write(str(hsp.sbjct) + "...\n")
+					out.write(str(hsp.query[:50]) + "...\n")
+					out.write(str(hsp.match[:50]) + "...\n")
+					out.write(str(hsp.sbjct[:50]) + "...\n")
 	result.close()
 #doBlast ("thrombin.fa")
 selectProt("blast.xml")
