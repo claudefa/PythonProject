9906e67758a0e6db0a77e4b932e6a19a8ba00c6a
Claudia <claudiafontsere@gmail.com>
2015-03-16 11:23:10 +0100

    adding mirrortree1

---
+++ Mirror_Tree_v1/setup.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+	name='MirrorTree',
+	version='1.0',
+	description='Use different types of correlation along evolution to identify interacting protein pairs',
+	author='Elba Raimúndez, Clàudia Fontserè, Lucas Michel',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Evolutionary Biologists',
+		'Programming Language :: Python',
+		'Topic :: Bioinformatics :: Phylogenetic trees'
+	],
+	keywords='Mirror trees',
+	packages = ['MirrorTree'],
+	package_dir={'MirrorTree': 'MirrorTree'},
+	py_modules = ['MirrorTree/modules', 'MirrorTree/functions'],
+	scripts = ['MirrorTree/mirrorTree'],
+)
